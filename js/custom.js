60fc6772680c0926e6c65d875f9e96e58cbb3007
Aiden Dickens <aiden.dickens@43540.auckland.natcoll.ac.nz>
2014-07-24 15:01:13 +1200

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+//Custom JQuery
+
+$(document).ready(function(){
+
+
+// alert('Welcome to the Stage 5 Website');
+
+// $('.intro-message > h1')
+// 		.hide(4000)
+		
+// 		.fadeout(4000);
+
+$('.intro-message >h1').hide()
+		.delay(1000)
+		.show(4000);
+
+$('.intro-message >h3').hide()
+		.delay(1500)
+		.show(4000);
+
+$('.laurels').hide()
+		.delay(1700)
+		.show(4000);
+
+$('#credits').hide()
+		.delay(2000)
+		.show(4000);
+
+});
+
